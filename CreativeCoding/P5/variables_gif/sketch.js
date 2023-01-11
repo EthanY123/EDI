710cf169d23ec686307cdf7d2afd3739cf310ecd
EthanY123 <113398036+EthanY123@users.noreply.github.com>
2023-01-11 09:38:17 -0500

    edit

--- CreativeCoding/P5/variables_gif/sketch.js
+++ CreativeCoding/P5/variables_gif/sketch.js
@@ -10,10 +10,10 @@ function setup() {
 
 function draw() {
   fill(132, 23,213);
-  strokeWeight(1);
+  strokeWeight(0);
 
   var num = 15;
-  var sideLen = windowWidth / num; 
+  var sideLen = windowWidth / num;
 
   for (var y = 0; y < windowWidth; y = y + sideLen) {
     for (var x = 0; x < windowWidth; x = x + sideLen) {
