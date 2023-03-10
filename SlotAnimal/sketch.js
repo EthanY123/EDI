f8490859a8585ccd7bbd0dfef6771a8df7df9105
EthanY123 <113398036+EthanY123@users.noreply.github.com>
2023-03-10 11:06:53 -0500

    add

---
+++ SlotAnimal/sketch.js
@@ -0,0 +1,30 @@
+let gif;
+
+function preload (){
+  bg = loadImage ('assets/backgroundimage.png')
+}
+
+function setup() {
+  createCanvas(windowWidth, windowWidth);
+}
+
+function draw() {
+  fill(132, 23,213);
+  strokeWeight(0);
+
+  var num = 15;
+  var sideLen = windowWidth/num;
+
+  translate (-150,  -150)
+
+  for (var y = 0; y < 2 * windowWidth; y = y + sideLen) {
+    for (var x = 0; x < 2 * windowWidth; x = x + sideLen) {
+      image(bg, x, y, windowWidth/num, windowWidth/num);
+    }
+  }
+}
+
+function windowResized () {
+  resizeCanvas (windowWidth, windowHeight)
+
+}
